e6f4d65ed3b495541dc6d6db24577fb409527a39
maxime <maxime.de.cock98@gmail.com>
2020-08-22 18:12:57 +0200

    mis à jour fichier ajoutUtilisateur.js

---
+++ FrontEnd/js/ajouterUtilisateur.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function ajoutUtilisateur(nom,prenom,pseudo,mdp){
+    xhr = new XMLHttpRequest();
+    xhr.open("get",`/ajouterUtilisateur?prenom=${prenom}$nom=${nom}&mdp=${mdp}`,true);
+    xhr.onerror =
+        function (){
+            console.log("ERROR  --  status: " + this.status + ", readyState: " + this.readyState);
+            // TODO AFFICHER MESSAGE D'ERREUR
+        };
+    xhr.onload =
+        function () {
+            let request = JSON.parse(this.responseText)[0];
+            if (request.status == 201){
+                document.getElementById("pseudoExisteDejaErreur").style.display= "none";
+                document.getElementById("submitBtn").style.backgroundColor= "green";
+                alert("Profile créé! Connectez-vous pour jouer.")
+                closPopUps();
+            }
+            else if (request.status == 400){
+                document.getElementById("pseudoInput").style.color= "red";
+                document.getElementById("pseudoExisteDejaErreur").style.display= "block";
+            }
+        };
+    xhr.send();
+}
--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,2 +1,10 @@
 "use strict";
 
+/*
+*
+*  Projet : organisateur personnel
+*  étudiant : Maxime De Cock
+*  début le 22/08/2020  à 16h30
+*
+*/
+
